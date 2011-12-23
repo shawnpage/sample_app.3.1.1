7e0b97381e5d6f8c39c66b8950bafe246d19122b
Shawn Page <bspage@webassign.net>
2011-12-23 12:55:27 -0500

    User signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,21 +44,21 @@ class User < ActiveRecord::Base
 
   private
 
-  def encrypt_password
-    self.salt = make_salt unless has_password?(password)
-    self.encrypted_password = encrypt(password)
-  end
+    def encrypt_password
+      self.salt = make_salt unless has_password?(password)
+      self.encrypted_password = encrypt(password)
+    end
 
-  def encrypt(string)
-    secure_hash("#{salt}--#{string}")
-  end
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
 
-  def make_salt
-    secure_hash("#{Time.now.utc}--#{password}")
-  end
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
 
-  def secure_hash(string)
-    Digest::SHA2.hexdigest(string)
-  end
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 
 end
