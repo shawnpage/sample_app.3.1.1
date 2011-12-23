bd5753d49d48822dc358501833db50995ae4f629
Shawn Page <bspage@webassign.net>
2011-12-23 14:10:52 -0500

    Finished ch8 exercises

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe "Users" do
 
   describe "signup" do
+
     describe "failure" do
+
       it "should not make a new user" do
         lambda do
           visit signup_path
@@ -16,6 +18,7 @@ describe "Users" do
           response.should have_selector("div#error_explanation")
         end.should_not change(User, :count)
       end
+
     end
 
     describe "success" do
@@ -31,8 +34,8 @@ describe "Users" do
           response.should have_selector("div.flash.success", :content => "Welcome")
           response.should render_template('users/show')
         end.should change(User, :count).by(1)
-      end
 
+      end
     end
   end
 end
